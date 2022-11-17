a126a26dfe5ee35c26035e9da416ae12387d6f8a
Hyperival <sanjaygautam779@gmail.com>
2022-11-17 16:46:05 +0530

    even or odd code is added

---
+++ OddAndEven/OddEven.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddAndEven
+{
+    class OddEven
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your number is: " + num);
+
+            if (num%2 == 0)
+            {
+                Console.WriteLine("number is even");
+
+            }
+            else
+            {
+                Console.WriteLine("number is odd");
+            }
+
+        }
+    }
+}
